a7c89747a1d31a7f0a05451abf7284e1931cb542
Delnia Alipour <delniaalipur@gmail.com>
2023-09-28 20:17:59 +0100

    add DataFetchingTest to Route path

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import DataFetchingTest from "./pages/DataFetchingTest";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
+		<Route path="/DataFetchingTest" element={<DataFetchingTest />} />
 		<Route path="/about/this/site" element={<About />} />
 	</Routes>
 );

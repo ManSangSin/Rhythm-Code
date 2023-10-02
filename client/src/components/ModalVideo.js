e6aa88462222326b7dbe5303f1809243a424ec63
Delnia Alipour <delniaalipur@gmail.com>
2023-10-02 20:22:11 +0100

    Show description in modal for rhythm
    check if a rhythm has just video, or just description or both

--- client/src/components/ModalVideo.js
+++ client/src/components/ModalVideo.js
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Nav from "react-bootstrap/Nav";
+import Tab from "react-bootstrap/Tab";
 import VideoCard from "./VideoCard";
 
-function ModalVideo({ title, url, location, show, handleClose }) {
+function ModalVideo({ title, url, description, show, handleClose, location }) {
+  const [activeTab, setActiveTab] = useState("video");
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
 
   return (
-    <>
-
-      <Modal show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}>
-        <Modal.Header closeButton>
-          <Modal.Title> {title} </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {location}
-          <VideoCard url={url} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{location}</p>
+        {url && description ? (
+          <Tab.Container activeKey={activeTab}>
+            <Nav variant="tabs">
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="video"
+                  onClick={() => handleTabChange("video")}
+                >
+                  Video
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="description"
+                  onClick={() => handleTabChange("description")}
+                >
+                  Description
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              <Tab.Pane eventKey="video">
+                <VideoCard url={url} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="description">{description}</Tab.Pane>
+            </Tab.Content>
+          </Tab.Container>
+        ) : (
+          <div>
+            {url && <VideoCard url={url} />}
+            {description && <div>Description: {description}</div>}
+          </div>
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary">Understood</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 

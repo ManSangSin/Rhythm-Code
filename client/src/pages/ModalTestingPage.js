10e361a1d5fc0e1c8c91e588c0cf12e0bc3f9fa6
Delnia Alipour <delniaalipur@gmail.com>
2023-10-02 20:28:46 +0100

    Update data and modal handling

--- client/src/pages/ModalTestingPage.js
+++ client/src/pages/ModalTestingPage.js
@@ -2,26 +2,66 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import ModalVideo from "../components/ModalVideo";
 
-function ModalTestingPage(){
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const data =
-        {
-            title: "Rumba Guaguanco",
-            url: "https://www.youtube.com/watch?v=gJVT_5swkhA",
-            location: "Havana, Cuba",
-        };
+const rhythms = [
+    {
+        title: "DC Hand dancing",
+        url: "https://www.youtube.com/watch?v=M6uM0qrjetQ",
+        location: "Washington DC",
+        description: "This is a description of rhythm of ....",
+    },
+    {
+        title: "Rumba Guaguanco",
+        location: "Havana, Cuba",
+        description: "This is a description of rhythm of ....",
+    },
+    {
+        title: "DC Hand dancing 2",
+        url: "https://www.youtube.com/watch?v=M6uM0qrjetQ",
+        location: "Washington DC",
+    },
+];
 
-    return <div>
-        <h2>Testing Modal</h2>
-        <Button variant="primary" onClick={handleShow}>
-            Explore Rhythm
-        </Button>
-        <ModalVideo show={show} handleClose={handleClose} title={data.title} url={data.url} location={data.location} />
+function ModalTestingPage() {
+  const [show, setShow] = useState(false);
+  const [selectedRhythm, setSelectedRhythm] = useState(null);
 
-        </div>;
+  const handleClose = () => {
+    setShow(false);
+    setSelectedRhythm(null);
+  };
 
+  const handleShow = (rhythm) => {
+    setSelectedRhythm(rhythm);
+    setShow(true);
+  };
+
+  return (
+    <div>
+          <h2>Testing Modal</h2>
+           {/* just add some temporary style */}
+          <div style={{ display: "flex", flexDirection: "column", gap: "10px", width:"12rem" }}>
+        {rhythms.map((rhythm, index) => (
+          <Button
+            key={index}
+            variant="primary"
+            onClick={() => handleShow(rhythm)}
+          >
+            Explore {rhythm.title}
+          </Button>
+        ))}
+      </div>
+      {selectedRhythm && (
+        <ModalVideo
+          show={show}
+          handleClose={handleClose}
+          title={selectedRhythm.title}
+          url={selectedRhythm.url}
+          description={selectedRhythm.description}
+          location={selectedRhythm.location}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ModalTestingPage;

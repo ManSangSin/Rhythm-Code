d3e2855491ec598b5558986247cc3a9be1b91140
christina-mifsud <christina.mifsud@gmail.com>
2023-10-03 23:03:14 +0100

    Co-authored-by: Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
@@ -6,72 +6,44 @@ import ModalVideo from "../components/ModalVideo";
 import map from "./img/map.jpg";
 import CityIcon from "./CityIcon";
 
+import rhythmsJsonData from "./rhythmsData.json";
+
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
+
 	const [show, setShow] = useState(false);
 	const [selectedIcon, setSelectedIcon] = useState(null);
-    const handleClose = () => setShow(false);
-    const handleShow = (videoInfo) => {
+
+	const handleClose = () => setShow(false);
+	const handleShow = (videoInfo) => {
 		setShow(true);
 		setSelectedIcon(videoInfo); // Use a state variable to store the selected video info
 	};
 
-	useEffect(() => {
-		fetch("/api")
-			.then((res) => {
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				return res.json();
-			})
-			.then((body) => {
-				setMessage(body.message);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
-
-const dataList = [
-		{   id: 1,
-			title: "DC Hand Dancing",
-			url: "https://www.youtube.com/watch?v=M6uM0qrjetQ",
-			location: "Washington DC",
-		},
-		{
-			id: 2,
-			title: "Rumba Guaguanco",
-			url: "https://www.youtube.com/watch?v=gJVT_5swkhA",
-			location: "Havana, Cuba",
-		},
-	];
-
 	return (
 		<main role="main">
 			<div>
+				<Link to="/DataFetchingTest">Fetch Data from backend</Link>
+
 				<img className="map" src={map} alt="North America Map" />
 
 				<div className="icons">
-					{dataList.map((dataItem) => (
+					{rhythmsJsonData.map((dataItem) => (
 						<div key={dataItem.id}>
-						<CityIcon
-							cityName={dataItem.location}
-							onClick={() => handleShow(dataItem)}
-						/>
+							<CityIcon
+								cityName={dataItem.location}
+								onClick={() => handleShow(dataItem)}
+							/>
 						</div>
 					))}
 					<ModalVideo
-					show={show}
-					handleClose={handleClose}
-					title={selectedIcon ? selectedIcon.title:""}
-					url={selectedIcon ?selectedIcon.url: ""}
-					location={selectedIcon ? selectedIcon.location:""} />
+						show={show}
+						handleClose={handleClose}
+						title={selectedIcon ? selectedIcon.title : ""}
+						url={selectedIcon ? selectedIcon.url : ""}
+						location={selectedIcon ? selectedIcon.location : ""}
+					/>
 				</div>
 
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-				<Link to="/DataFetchingTest">Fetch Data from backend</Link>
 				<br></br>
 				<Link to="/about/this/site">About</Link>
 			</div>

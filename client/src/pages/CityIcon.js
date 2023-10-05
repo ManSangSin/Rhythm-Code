3f84e130ab5bd8031e47d50f05e5ad15c4a96cc5
christina-mifsud <christina.mifsud@gmail.com>
2023-10-05 21:40:44 +0100

    Deleted json files

--- client/src/pages/CityIcon.js
+++ client/src/pages/CityIcon.js
@@ -1,17 +1,17 @@
 
 // city styling classes
 const cityStyles = {
-		"havana-cuba": {
-			position: "absolute",
-			top: "795px",
-			left: "570px",
-		},
-		"washington-dc": {
-			position: "absolute",
-			top: "620px",
-			left: "570px",
-		},
-	};
+	"havana-cuba": {
+		position: "absolute",
+		top: "795px",
+		left: "570px",
+	},
+	"washington-dc": {
+		position: "absolute",
+		top: "620px",
+		left: "570px",
+	},
+};
 
 const CityIcon = ({ cityName, onClick }) => {
   const cityStyle = cityStyles[cityName.toLowerCase().replace(/[,\s]+/g, "-")]; // to remove any spaces, commas etc. from location data eg. "Havana, Cuba" to match our styling classes above
@@ -21,7 +21,7 @@ const CityIcon = ({ cityName, onClick }) => {
     <div style={cityStyle}>
       <svg onClick={onClick}>
         <circle cx="10" cy="10" r="10" />
-      </svg>;
+      </svg>
     </div>
   );
 };

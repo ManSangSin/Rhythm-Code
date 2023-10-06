b8b747bb4c5cf595ccd8de33d70baa176b4e9134
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-06 11:36:03 +0100

    created new table called locations
    table includes location name
    linked to rhythm tables using location id

--- server/db/build-rhythms.sql
+++ server/db/build-rhythms.sql
@@ -3,8 +3,14 @@
 
 DROP TABLE IF EXISTS rhythms;
 
+CREATE TABLE locations (
+    id             SERIAL NOT NULL PRIMARY KEY,
+    location       VARCHAR(255)
+);
+
 CREATE TABLE rhythms (
     id		        SERIAL NOT NULL PRIMARY KEY,
+    location_id     INTEGER REFERENCES locations (id),
     rhythm	        VARCHAR(255),
     date            YEAR,
     video		    VARCHAR(512),

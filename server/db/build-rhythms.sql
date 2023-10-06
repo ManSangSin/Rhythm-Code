e8ef00a66753a57f0a4293ae0f0286d5f0b9d066
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-06 11:33:14 +0100

    updated rhythms table
    renamed url to video
    included columns for audio and text description
    included column for date

--- server/db/build-rhythms.sql
+++ server/db/build-rhythms.sql
@@ -4,10 +4,11 @@
 DROP TABLE IF EXISTS rhythms;
 
 CREATE TABLE rhythms (
-    id		    serial PRIMARY KEY,
-    title	    VARCHAR(255),
-    url		    VARCHAR(512),
-    location	VARCHAR(255)
+    id		        SERIAL NOT NULL PRIMARY KEY,
+    rhythm	        VARCHAR(255),
+    date            YEAR,
+    video		    VARCHAR(512),
+    audio           VARCHAR(512),
+    description     TEXT
 );
 
-INSERT INTO rhythms (title, url, location) VALUES('DC Hand dancing','https://www.youtube.com/watch?v=M6uM0qrjetQ','washington DC'), ('Rumba Guaguanco','https://www.youtube.com/watch?v=gJVT_5swkhA','Havana, Cuba');

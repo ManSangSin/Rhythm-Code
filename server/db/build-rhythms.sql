532b78980397b225f8a04f2386e91b8049528224
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-06 14:50:31 +0100

    added constraints to foreign keys

--- server/db/build-rhythms.sql
+++ server/db/build-rhythms.sql
@@ -26,19 +26,24 @@ CREATE TABLE regions (
 CREATE TABLE locations (
     id             SERIAL NOT NULL PRIMARY KEY,
     location       VARCHAR(255),
-    mapping_id     INTEGER REFERENCES mappings (id),
-    region_id      INTEGER REFERENCES regions (id)
+    mapping_id     INTEGER,
+    region_id      INTEGER,
+    CONSTRAINT     loc_mapping_fk FOREIGN KEY (mapping_id) REFERENCES mappings (id),
+    CONSTRAINT     loc_region_fk  FOREIGN KEY (region_id) REFERENCES regions(id)
 );
 
 CREATE TABLE rhythms (
     id		        SERIAL NOT NULL PRIMARY KEY,
-    rhythm_code_id  INTEGER REFERENCES rhythm_codes (id),
-    location_id     INTEGER REFERENCES locations (id),
+    rhythm_code_id  INTEGER,
+    location_id     INTEGER,
     rhythm	        VARCHAR(255),
     language        VARCHAR(255),
-    year_start      YEAR,
-    year_end        YEAR,
+    year_start      INTEGER,
+    year_end        INTEGER,
     video		    VARCHAR(512),
     audio           VARCHAR(512),
-    description     TEXT
+    description     TEXT,
+    CONSTRAINT      rhy_rhythm_code_fk FOREIGN KEY (rhythm_code_id) REFERENCES rhythm_codes (id),
+    CONSTRAINT      rhy_location_fk  FOREIGN KEY (location_id) REFERENCES locations (id)
 );
+

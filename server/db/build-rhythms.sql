713792917537ffb77b4ee151cbdc691d2e0752fa
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-06 18:03:50 +0100

    adding some dummy data points for mappings table
    added data for Guedeloupe and NOLA

--- server/db/build-rhythms.sql
+++ server/db/build-rhythms.sql
@@ -48,11 +48,11 @@ CREATE TABLE rhythms (
 );
 
 INSERT INTO rhythm_codes (rhythm_code) VALUES('Rumba'), ('Gwo-Ka'), ('Second Line'), ('Gwo-Ka'), ('Blues / Early Jazz / Trad Jazz'), ('Swing');
-INSERT INTO mappings (lon, lat) VALUES(570, 795), (570, 620);
+INSERT INTO mappings (lon, lat) VALUES(570, 795), (650, 580), (540, 820), (570, 620);
 INSERT INTO regions (region) VALUES('Carribean'), ('USA');
-INSERT INTO locations (location) VALUES('Cuba'), ('Guadeloupe'), ('NOLA'), ('Washington DC');
-INSERT INTO rhythms (rhythm, language, year_start, year_end, video, description) VALUES('Columbia', 'Spanish', 1880, 2020, 'https://youtu.be/W4aMl4HpKIQ','Columbia: rápida y solo para hombres, solo tambores y no se canta');
-INSERT INTO rhythms (rhythm, language, year_start, year_end, video, description) VALUES('Yambu', 'Spanish', 1880, 2020, 'https://youtu.be/Mux8ibZWbrg','Yambu: más lento, en pareja (and heteronormative) but traditionally not touching');
-INSERT INTO rhythms (rhythm, language, year_start, year_end, video, description) VALUES('Guaguanco', 'Spanish', 1880, 2020, 'https://youtu.be/gJVT_5swkhA','Guaguanco: faster, most popular and well known : similar flirtatious narrative and motifs to Yambu');
-INSERT INTO rhythms (rhythm, language, year_start, year_end, video, description) VALUES('Habanera', 'Spanish', 1880, 2020, 'https://youtu.be/iLGuLEqtq7g','Habanera: more baile de salon');
-INSERT INTO rhythms (rhythm, language, year_start, year_end, audio, description) VALUES('Graj', 'French', 1880, 2020, 'https://drive.google.com/file/d/1MoZXjAO2cqPU3IssoP40PYrAsshT4PVm/view','The graj accompanies the agricultural work');
+INSERT INTO locations (location, mapping_id, region_id) VALUES('Cuba', 1, 1), ('Guadeloupe', 2, 1), ('NOLA', 3, 2), ('Washington DC', 4, 2);
+INSERT INTO rhythms (rhythm_code_id, location_id, rhythm, language, year_start, year_end, video, description) VALUES(1, 1, 'Columbia', 'Spanish', 1880, 2020, 'https://youtu.be/W4aMl4HpKIQ','Columbia: rápida y solo para hombres, solo tambores y no se canta');
+INSERT INTO rhythms (rhythm_code_id, location_id, rhythm, language, year_start, year_end, video, description) VALUES(1, 1, 'Yambu', 'Spanish', 1880, 2020, 'https://youtu.be/Mux8ibZWbrg','Yambu: más lento, en pareja (and heteronormative) but traditionally not touching');
+INSERT INTO rhythms (rhythm_code_id, location_id, rhythm, language, year_start, year_end, video, description) VALUES(1, 1, 'Guaguanco', 'Spanish', 1880, 2020, 'https://youtu.be/gJVT_5swkhA','Guaguanco: faster, most popular and well known : similar flirtatious narrative and motifs to Yambu');
+INSERT INTO rhythms (rhythm_code_id, location_id, rhythm, language, year_start, year_end, video, description) VALUES(1, 1, 'Habanera', 'Spanish', 1880, 2020, 'https://youtu.be/iLGuLEqtq7g','Habanera: more baile de salon');
+INSERT INTO rhythms (rhythm_code_id, location_id, rhythm, language, year_start, year_end, audio, description) VALUES(2, 2, 'Graj', 'French', 1700, 2020, 'https://drive.google.com/file/d/1MoZXjAO2cqPU3IssoP40PYrAsshT4PVm/view','The graj accompanies the agricultural work');

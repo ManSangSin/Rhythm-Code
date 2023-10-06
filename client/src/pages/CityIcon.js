267005f0fabe79b3383247bfb9345b8ed9a16b9d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-06 18:23:03 +0100

    updated CityIcon component to use prop
    prop is passed down from home.js (parent) for lat and lon data which is used instead of the hardcoded data from cityStyles
    moved styling to inline

--- client/src/pages/CityIcon.js
+++ client/src/pages/CityIcon.js
@@ -1,24 +1,11 @@
 
 // city styling classes
-const cityStyles = {
-	"havana-cuba": {
-		position: "absolute",
-		top: "795px",
-		left: "570px",
-	},
-	"washington-dc": {
-		position: "absolute",
-		top: "620px",
-		left: "570px",
-	},
-};
 
-const CityIcon = ({ cityName, onClick }) => {
-  const cityStyle = cityStyles[cityName.toLowerCase().replace(/[,\s]+/g, "-")]; // to remove any spaces, commas etc. from location data eg. "Havana, Cuba" to match our styling classes above
+const CityIcon = ({ cityName, onClick, lon, lat }) => {
 
 	return (
 	// applying city styling classes to div
-    <div style={cityStyle}>
+    <div style={{ position: "absolute", top: lat, left: lon }}>
       <svg onClick={onClick}>
         <circle cx="10" cy="10" r="10" />
       </svg>

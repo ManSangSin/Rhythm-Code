b02d8edf0bcee97c98e58af44ac903a5604a9d56
Delnia Alipour <delniaalipur@gmail.com>
2023-10-06 20:50:00 +0100

    add SliderTest to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import ModalTestingPage from "./pages/ModalTestingPage";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import SliderTest from "./pages/SliderTest";
 import DataFetchingTest from "./pages/DataFetchingTest";
 
 
 const App = () => (
 	<>
 	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/modaltesting" element={<ModalTestingPage />} />
+	<Route path="/" element={<Home />} />
+		<Route path="/SliderTest" element={<SliderTest />} />
+		<Route path="/ModalTestingPage" element={<ModalTestingPage />} />
 		<Route path="/about/this/site" element={<About />} />
 		<Route path="/DataFetchingTest" element={<DataFetchingTest />} />
 	</Routes>

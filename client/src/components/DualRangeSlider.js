d4da8bf083ad9b7bbddf9df611c7770bdba545a9
Delnia Alipour <delniaalipur@gmail.com>
2023-10-06 20:40:42 +0100

    Create a slider

---
+++ client/src/components/DualRangeSlider.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Range } from "react-range";
+
+const DualRangeSlider = ({ selectedRange, onChangeRange }) => {
+	const min = 1400;
+	const max = 2023;
+	const step = 50;
+
+	const handleChange = (newValues) => {
+		onChangeRange(newValues);
+	};
+
+	const railLabels = [1500, 1600, 1700, 1800, 1900];
+
+	return (
+		<div style={{ margin: "20px", maxWidth: "400px" }}>
+			<h2>
+				Range: {selectedRange[0]} - {selectedRange[1]}
+			</h2>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					position: "relative",
+				}}
+			>
+				<Range
+					step={step}
+					min={min}
+					max={max}
+					values={selectedRange}
+					onChange={handleChange}
+					renderTrack={({ props, children }) => (
+						<div
+							{...props}
+							style={{
+								...props.style,
+								height: "6px",
+								width: "100%",
+								backgroundColor: "#ddd",
+								position: "relative",
+							}}
+						>
+							{children}
+							{railLabels.map((label, index) => (
+								<div
+									key={index}
+									style={{
+										position: "absolute",
+										bottom: "-25px",
+										left: `${((label - min) / (max - min)) * 100}%`,
+										transform: "translateX(-50%)",
+									}}
+								>
+									{label}
+								</div>
+							))}
+						</div>
+					)}
+					renderThumb={({ props }) => (
+						<div
+							{...props}
+							style={{
+								...props.style,
+								height: "20px",
+								width: "20px",
+								backgroundColor: "#007bbb",
+								borderRadius: "50%",
+								outline: "none",
+							}}
+						/>
+					)}
+				/>
+			</div>
+			<div style={{ display: "flex", justifyContent: "space-between" }}>
+				<span>{min}</span>
+				<span>{max}</span>
+			</div>
+		</div>
+	);
+};
+
+export default DualRangeSlider;
+// src: https://codesandbox.io/s/multi-range-slider-react-js-forked-4uq1uo?file=/src/component/multiRangeSlider/MultiRangeSlider.js:529-589

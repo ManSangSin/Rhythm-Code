d6c97ff857b0e2f6184df2612caf9ccd41441f6b
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-07 17:32:14 +0100

    testing merge/rebase

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,72 +1,59 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import "./Home.css";
 import ModalVideo from "../components/ModalVideo";
 import map from "./img/map.jpg";
 import CityIcon from "./CityIcon";
-
 export function Home() {
-
-	const [show, setShow] = useState(false);
-	const [selectedIcon, setSelectedIcon] = useState(null);
-
+    const [show, setShow] = useState(false);
+    const [selectedIcon, setSelectedIcon] = useState(null);
     const [rhythms, setRhythms] = useState([]);
-
-		const API_URL = "/api/rhythms";
-
-		useEffect(() => {
-			fetch(API_URL)
-				.then((response) => {
-					return response.json();
-				})
-				.then((data) => {
-					console.log("Fetched Data:", data);
-					setRhythms(data);
-				})
-				.catch((error) => {
-					console.error("Error fetching data:", error);
-				});
-		}, []);
-
-
-	const handleClose = () => setShow(false);
-	const handleShow = (videoInfo) => {
-		setShow(true);
-		setSelectedIcon(videoInfo); // Use a state variable to store the selected video info
-	};
-
-
-	return (
-		<main role="main">
-			<div>
-				<img className="map" src={map} alt="North America Map" />
-
-				<div className="icons">
-					{rhythms.map((dataItem) => (
-						<div key={dataItem.id}>
-							<CityIcon
-								cityName={dataItem.location}
-								lon={dataItem.lon}
-								lat={dataItem.lat}
-								onClick={() => handleShow(dataItem)}
-							/>
-						</div>
-					))}
-					<ModalVideo
-						show={show}
-						handleClose={handleClose}
-						title={selectedIcon ? selectedIcon.rhythm : ""}
-						url={selectedIcon ? selectedIcon.video : ""}
-						location={selectedIcon ? selectedIcon.location : ""}
-					/>
-				</div>
-
-				<br></br>
-				<Link to="/about/this/site">About</Link>
-			</div>
-		</main>
-	);
+        const API_URL = "/api/rhythms";
+        useEffect(() => {
+            fetch(API_URL)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log("Fetched Data:", data);
+                    setRhythms(data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching data:", error);
+                });
+        }, []);
+    const handleClose = () => setShow(false);
+    const handleShow = (videoInfo) => {
+        setShow(true);
+        setSelectedIcon(videoInfo); // useState to store the selected video info
+    };
+    return (
+        <main role="main">
+            <div>
+                <img className="map" src={map} alt="North America Map" />
+                <div className="icons">
+                    {rhythms.map((dataItem) => (
+                        <div key={dataItem.id}>
+                            <CityIcon
+                                cityName={dataItem.location}
+                                onClick={() => handleShow(dataItem)}
+                            />
+                        </div>
+                    ))}
+                    <ModalVideo
+                        show={show}
+                        handleClose={handleClose}
+                        title={selectedIcon ? selectedIcon.title : ""}
+                        url={selectedIcon ? selectedIcon.url : ""}
+                        location={selectedIcon ? selectedIcon.location : ""}
+                    />
+                </div>
+                <br></br>
+                <Link to="/about/this/site">About</Link>
+                <br></br>
+                <Link to="/RhythmsPanelTestingPage">RhythmsPanelTestingPage</Link>
+            </div>
+        </main>
+    );
 }
-
 export default Home;

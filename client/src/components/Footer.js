4121eeccdfe9ef7dee4560fa6f0337dd212ee07c
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-13 13:33:28 +0100

    added text and title for organisation section

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
     return (
         <footer>
-            <span>I am a footer</span>
+            <h3>decodenoir.org</h3>
+            <p>
+                This interactive map is part an ethnography exhibition concept that aims to seed a coherent African Diaspora identity narrative in hearts and minds worldwide
+            </p>
+            <h3>codeyourfuture.io</h3>
+            <p>
+                A UK based non-profit organisation that trains refugees & disadvantaged people to become web developers and helps them to find work in the tech industry.
+            </p>
         </footer>
     );
 };

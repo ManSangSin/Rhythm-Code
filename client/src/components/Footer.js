a69e0f8b717852661691e4d9ff54dbd6d05ef790
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-13 18:02:39 +0100

    created css file and imported into Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Footer.css";
 import OrganisationLink from "./OrganisationLink";
 import linkedinIcon from "../assets/linkedin-icon.svg";
 import githubIcon from "../assets/github-icon.svg";
@@ -7,13 +8,17 @@ import ExternalLink from "./ExternalLink";
 const Footer = () => {
     return (
         <footer>
-            <OrganisationLink organisationName="decodenoir.org" organisationText="This interactive map is part an ethnography exhibition concept that aims to seed a coherent African Diaspora identity narrative in hearts and minds worldwide" />
-            <OrganisationLink organisationName="codeyourfuture.io" organisationText="A UK based non-profit organisation that trains refugees & disadvantaged people to become web developers and helps them to find work in the tech industry." />
-            <ExternalLink companyIcon={linkedinIcon} text="Christina Mifsud" />
-            <ExternalLink companyIcon={linkedinIcon} text="Delnia Alipour" />
-            <ExternalLink companyIcon={linkedinIcon} text="Irianni Munoz" />
-            <ExternalLink companyIcon={linkedinIcon} text="Man Sang Sin" />
-            <ExternalLink companyIcon={githubIcon} text="See source code on Github" />
+            <div>
+                <OrganisationLink organisationName="decodenoir.org" organisationText="This interactive map is part an ethnography exhibition concept that aims to seed a coherent African Diaspora identity narrative in hearts and minds worldwide" />
+                <OrganisationLink organisationName="codeyourfuture.io" organisationText="A UK based non-profit organisation that trains refugees & disadvantaged people to become web developers and helps them to find work in the tech industry." />
+            </div>
+            <div>
+                <ExternalLink companyIcon={linkedinIcon} text="Christina Mifsud" />
+                <ExternalLink companyIcon={linkedinIcon} text="Delnia Alipour" />
+                <ExternalLink companyIcon={linkedinIcon} text="Irianni Munoz" />
+                <ExternalLink companyIcon={linkedinIcon} text="Man Sang Sin" />
+                <ExternalLink companyIcon={githubIcon} text="See source code on Github" />
+            </div>
         </footer>
     );
 };

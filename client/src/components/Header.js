938d440367eadb49ef0b05dc30dfb1b275b94855
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-13 09:48:23 +0100

    anchor for elements which link to websites

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,22 @@ import React from "react";
 import cross from "../assets/cross.svg";
 import logo from "../assets/cyf_logo.png";
 
+const decodenoir_url = "https://www.decodenoir.org/";
+const cyf_url = "https://codeyourfuture.io/";
+
+
 const Header = () => {
     return (
         <header>
-            <span>decodenoir.org</span>
-            <img src={cross} alt="cross icon" />
-            <img src={logo} alt="cyf logo" />
+            <div className="header-logo_container">
+                <a href={decodenoir_url}>
+                    <span>decodenoir.org</span>
+                </a>
+                <img src={cross} alt="cross icon" />
+                <a href={cyf_url}>
+                    <img src={logo} alt="cyf logo" />
+                </a>
+            </div>
         </header>
     );
 };

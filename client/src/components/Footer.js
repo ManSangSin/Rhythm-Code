720e49cd8f454f23ce276145f67d187b1da8e733
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-13 18:24:33 +0100

    used flex (column) for external links section

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,12 +12,16 @@ const Footer = () => {
                 <OrganisationLink organisationName="decodenoir.org" organisationText="This interactive map is part an ethnography exhibition concept that aims to seed a coherent African Diaspora identity narrative in hearts and minds worldwide" />
                 <OrganisationLink organisationName="codeyourfuture.io" organisationText="A UK based non-profit organisation that trains refugees & disadvantaged people to become web developers and helps them to find work in the tech industry." />
             </div>
-            <div>
-                <ExternalLink companyIcon={linkedinIcon} text="Christina Mifsud" />
-                <ExternalLink companyIcon={linkedinIcon} text="Delnia Alipour" />
-                <ExternalLink companyIcon={linkedinIcon} text="Irianni Munoz" />
-                <ExternalLink companyIcon={linkedinIcon} text="Man Sang Sin" />
-                <ExternalLink companyIcon={githubIcon} text="See source code on Github" />
+            <div className="external-links-container">
+                <div>
+                    <ExternalLink companyIcon={linkedinIcon} text="Christina Mifsud" />
+                    <ExternalLink companyIcon={linkedinIcon} text="Delnia Alipour" />
+                    <ExternalLink companyIcon={linkedinIcon} text="Irianni Munoz" />
+                    <ExternalLink companyIcon={linkedinIcon} text="Man Sang Sin" />
+                </div>
+                <div>
+                    <ExternalLink companyIcon={githubIcon} text="See source code on Github" />
+                </div>
             </div>
         </footer>
     );

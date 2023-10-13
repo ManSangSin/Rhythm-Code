c409c1ac8fa726289e20f78d948536615c9ae4f0
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-13 17:37:07 +0100

    created team section with data and images provided

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import OrganisationLink from "./OrganisationLink";
+import linkedinIcon from "../assets/linkedin-icon.svg";
+import githubIcon from "../assets/github-icon.svg";
 
 const Footer = () => {
     return (
         <footer>
             <OrganisationLink organisationName="decodenoir.org" organisationText="This interactive map is part an ethnography exhibition concept that aims to seed a coherent African Diaspora identity narrative in hearts and minds worldwide" />
             <OrganisationLink organisationName="codeyourfuture.io" organisationText="A UK based non-profit organisation that trains refugees & disadvantaged people to become web developers and helps them to find work in the tech industry." />
+            <p>Engineered by Team Rhythm Code</p>
+            <p><img src={linkedinIcon} alt="linkedin company logo" />Christina Mifsud</p>
+            <p><img src={linkedinIcon} alt="linkedin company logo" />Delnia Alipour</p>
+            <p><img src={linkedinIcon} alt="linkedin company logo" />Irianni Munoz</p>
+            <p><img src={linkedinIcon} alt="linkedin company logo" />Man Sang Sin</p>
+            <p><img src={githubIcon} alt="github company logo" />See source code on Github</p>
         </footer>
     );
 };

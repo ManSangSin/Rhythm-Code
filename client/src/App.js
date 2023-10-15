d5e2af38856cb741e1ff0349fa68462535534b70
Delnia Alipour <delniaalipur@gmail.com>
2023-10-16 00:59:39 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,24 @@ import ModalTestingPage from "./pages/ModalTestingPage";
 import Home from "./pages/Home";
 import MapSection from "./components/MapSection";
 import SliderTest from "./pages/SliderTest";
-import DataFetchingTest from "./pages/DataFetchingTest";
 
 import RhythmsPanelTestingPage from "./pages/RhythmsPanelTestingPage";
 
-
-
 const App = () => (
 	<>
-	<Routes>
-	<Route path="/" element={<Home />} />
-		<Route path="/SliderTest" element={<SliderTest />} />
-		<Route path="/ModalTestingPage" element={<ModalTestingPage />} />
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-		<Route path="/ModalTestingPage" element={<ModalTestingPage />} />
-		<Route path="/RhythmsPanelTestingPage" element={<RhythmsPanelTestingPage />} />
-		<Route path="/MapSection" element={<MapSection />}></Route>
-	</Routes>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/SliderTest" element={<SliderTest />} />
+			<Route path="/ModalTestingPage" element={<ModalTestingPage />} />
+			<Route path="/" element={<Home />} />
+			<Route path="/about/this/site" element={<About />} />
+			<Route path="/ModalTestingPage" element={<ModalTestingPage />} />
+			<Route
+				path="/RhythmsPanelTestingPage"
+				element={<RhythmsPanelTestingPage />}
+			/>
+			<Route path="/MapSection" element={<MapSection />}></Route>
+		</Routes>
 	</>
 );
 

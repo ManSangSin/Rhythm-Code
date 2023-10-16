7df8a0b9b279ec07ab154d39ea723493fe15b57c
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-10-16 17:58:33 +0100

    separate CSS file created and content pass through object

--- client/src/components/ArtistSection.js
+++ client/src/components/ArtistSection.js
@@ -2,18 +2,26 @@ import React from "react";
 import Group from "../images/Group.png";
 import Deidre from "../images/deidre_profile.png";
 import Dot from "../images/dot_background.png";
+import "./ArtistSection.css";
+
+const content={
+    title:"Artist & Dance Researcher Deidre Molloy",
+    subtitle: "Doctoral student at University College Cork",
+    paragraph: "Deirdre has always danced. Her research centres dance as a decolonizing resource of collective knowledge and innovation.This Afrocentric arts research is suited to film, photography, music and performance collaborations – so be in touch!",
+    learnLink: "https://www.ucc.ie/en/fmt/doctoralschool/phdcandidates/#deirdre-molloy",
+    contactLink: "https://www.linkedin.com/in/deirdre-clare-molloy/",
+};
+
 function ArtistSection(){
     return <div className="artist">
         <div className="text">
-            <h4>Artist & Dance Researcher Deidre Molloy</h4>
-            <span>Doctoral student at University College Cork</span>
-            <p>Deirdre has always danced. Her research centres dance as a decolonizing resource of collective knowledge and innovation.
-                This Afrocentric arts research is suited to film, photography, music and performance collaborations – so be in touch!
-            </p>
-            <a href="https://www.ucc.ie/en/fmt/doctoralschool/phdcandidates/#deirdre-molloy">
+            <h4>{content.title}</h4>
+            <span>{content.subtitle}</span>
+            <p>{content.paragraph}</p>
+            <a href={content.learnLink}>
             <button className="learn">Learn more</button>
             </a>
-            <a href="https://www.linkedin.com/in/deirdre-clare-molloy/">
+            <a href={content.contactLink}>
                 <button className="contact"><img src={Group} alt="envelope"></img>Get in touch</button>
             </a>
         </div>

eb79ced6a2a0ce358b4326a52733abef5a81e288
Delnia Alipour <delniaalipur@gmail.com>
2023-10-16 21:05:24 +0100

    updated the content

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,33 +1,32 @@
 import React from "react";
 import "./Hero.css";
 import heroImage from "../images/heroImage.png";
-
-const content = {
-	text1: (
-		<div className="subheader">
-			<h1>Black Atlantic Space-Time</h1>
-			<h2>A journey through ancestral rhythms</h2>
-		</div>
-	),
-	text2: (
-		<div className="quote">
-			<p className="sentence">
-				“Africanist rhythms unify present, past and future in my body.”
-			</p>
-			<p className="name">- Deirdre Molloy</p>
-		</div>
-	),
+const Content = {
+	text1: {
+		h1: "Black Atlantic Space-Time",
+		h2: "A journey through ancestral rhythms",
+	},
+	text2: {
+		quote: "“Africanist rhythms unify present, past and future in my body.”",
+		name: "- Deirdre Molloy",
+	},
 };
 
 const Hero = () => {
 	return (
 		<div className="hero-container">
 			<div className="hero-text">
-				{content.text1}
-				{content.text2}
+				<div className="subheader">
+					<h1>{Content.text1.h1}</h1>
+					<h2>{Content.text1.h2}</h2>
+				</div>
+				<div className="quote">
+					<p className="sentence">{Content.text2.quote}</p>
+					<p className="name">{Content.text2.name}</p>
+				</div>
 			</div>
 			<div className="hero-image">
-				<img src={heroImage} alt="dancer"></img>
+				<img src={heroImage} alt="dancer" />
 			</div>
 		</div>
 	);

9e85efaa2c8786304046347bf1ca44e6eacbd45d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-16 07:23:40 +0100

    moved ExternalLink styling to separate CSS

--- client/src/components/ExternalLink.js
+++ client/src/components/ExternalLink.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./ExternalLink.css";
 
 const ExternalLinks = ({ companyIcon, text, linkUrl }) => {
 	return (
 		// rel needs to be specified when target is used as security risk for older browsers
 		<a href={linkUrl} target="_blank" rel="noreferrer">
-			<p className="link">
-				<img className="link-icon" src={companyIcon} alt="company logo" />
+			<p className="externalLink">
+				<img
+					className="externalLink-icon"
+					src={companyIcon}
+					alt="company logo"
+				/>
 				{text}
 			</p>
 		</a>

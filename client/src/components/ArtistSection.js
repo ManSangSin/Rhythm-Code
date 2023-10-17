fb71f670521abf3eea4889f11a4e9628dd70cf36
christina-mifsud <christina.mifsud@gmail.com>
2023-10-18 00:52:43 +0100

    Responsive - ArtistSection

--- client/src/components/ArtistSection.js
+++ client/src/components/ArtistSection.js
@@ -4,32 +4,40 @@ import Deidre from "../images/deidre_profile.png";
 import Dot from "../images/dot_background.png";
 import "./ArtistSection.css";
 
-const content={
-    title:"Artist & Dance Researcher Deidre Molloy",
-    subtitle: "Doctoral student at University College Cork",
-    paragraph: "Deirdre has always danced. Her research centres dance as a decolonizing resource of collective knowledge and innovation.This Afrocentric arts research is suited to film, photography, music and performance collaborations – so be in touch!",
-    learnLink: "https://www.ucc.ie/en/fmt/doctoralschool/phdcandidates/#deirdre-molloy",
-    contactLink: "https://www.linkedin.com/in/deirdre-clare-molloy/",
+const content = {
+	title: "Artist & Dance Researcher Deidre Molloy",
+	subtitle: "Doctoral student at University College Cork",
+	paragraph:
+		"Deirdre has always danced. Her research centres dance as a decolonising resource of collective knowledge and innovation.This Afrocentric arts research is suited to film, photography, music and performance collaborations – so be in touch!",
+	learnLink:
+		"https://www.ucc.ie/en/fmt/doctoralschool/phdcandidates/#deirdre-molloy",
+	contactLink: "https://www.linkedin.com/in/deirdre-clare-molloy/",
 };
 
-function ArtistSection(){
-    return <div className="artist-section">
-        <div className="artist-text-container">
-            <h4>{content.title}</h4>
-            <span>{content.subtitle}</span>
-            <p>{content.paragraph}</p>
-            <a href={content.learnLink}>
-            <button className="artist-learn">Learn more</button>
-            </a>
-            <a href={content.contactLink}>
-                <button className="artist-contact"><img src={Group} alt="envelope"></img>Get in touch</button>
-            </a>
-        </div>
-        <div className="artist-image-container" >
-            <img className="artist-backgroundImage" src={Dot} alt="dots"></img>
-            <img className="artist-portrait" src={Deidre} alt="Deidre Molloy"></img>
-        </div>
-    </div>;
+function ArtistSection() {
+	return (
+		<div className="artist-section">
+			<div className="artist-text-container">
+				<h4>{content.title}</h4>
+				<span>{content.subtitle}</span>
+				<p>{content.paragraph}</p>
+				<div className="artist-buttons-container">
+					<a href={content.learnLink}>
+						<button className="artist-learn">Learn more</button>
+					</a>
+					<a href={content.contactLink}>
+						<button className="artist-contact">
+							<img src={Group} alt="envelope"></img>Get in touch
+						</button>
+					</a>
+				</div>
+			</div>
+			<div className="artist-image-container">
+				<img className="artist-backgroundImage" src={Dot} alt="dots"></img>
+				<img className="artist-portrait" src={Deidre} alt="Deidre Molloy"></img>
+			</div>
+		</div>
+	);
 }
 
 export default ArtistSection;

88fb5e07b5bf6c9a401ab5c81c78111d62c49b10
christina-mifsud <christina.mifsud@gmail.com>
2023-10-20 15:38:36 +0100

    updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,16 @@
 import "./Home.css";
 import Header from "../components/Header";
-// import Hero from "../components/Hero";
+import Hero from "../components/Hero";
 import MapSection from "../components/MapSection";
 import AboutSection from "../components/AboutSection";
 import ArtistSection from "../components/ArtistSection";
 import Footer from "../components/Footer";
-import MyMap from "../components/MyMap";
 
 function Home() {
 	return (
 		<main role="main">
 			<Header />
-			{/* <Hero /> */}
+			<Hero />
 			<MapSection />
 			<AboutSection />
 			<ArtistSection />

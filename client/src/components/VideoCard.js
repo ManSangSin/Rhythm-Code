2a77c20c8980f5f7ba46585deba554c670328b7d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-10-26 22:57:36 +0100

    filter id based on input url
    checks if url includes "youtu" or "google"
    filter id depending on source
    returns videorurl which is used in iframe

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -1,21 +1,33 @@
 import React from "react";
 
-function VideoCard({ url }){
-    //change how video url is split depending on input url to get video id
-    const urlId = url.includes("v=") ? url.split("v=")[1]?.substring(0, 11) : url.split(".be/")[1]?.substring(0, 11);
+function VideoCard({ url }) {
+	let urlId = "";
+	let videoUrl = "";
+	//change how video url is split depending on input url to get video id
+	if (url.includes("youtu")) {
+		urlId = url.includes("v=")
+			? url.split("v=")[1]?.substring(0, 11)
+			: url.split(".be/")[1]?.substring(0, 11);
+		videoUrl = `https://www.youtube.com/embed/${urlId}`;
+	}
+	if (url.includes("google")) {
+		urlId = url.split("file/d/")[1]?.substring(0, 33);
+		videoUrl = `https://drive.google.com/file/d/${urlId}/preview`;
+	}
 
-    return <div className="card">
-        <iframe
-          className="card-thumb"
-          width="100%"
-          height="383"
-          src={`https://www.youtube.com/embed/${urlId}`}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-        </div>;
-
+	return (
+		<div className="card">
+			<iframe
+				className="card-thumb"
+				width="100%"
+				height="383"
+				src={videoUrl}
+				title="YouTube video player"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowFullScreen
+			></iframe>
+		</div>
+	);
 }
 
 export default VideoCard;
